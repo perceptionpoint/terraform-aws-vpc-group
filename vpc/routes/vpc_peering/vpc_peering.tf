5564a1bacaf8dfe983260bdb7e38fee22e24587e
Oron Gola <oron.gola@perception-point.io>
2024-01-22 09:45:51 +0200

    fix reference to peer_vpc variable

--- vpc/routes/vpc_peering/vpc_peering.tf
+++ vpc/routes/vpc_peering/vpc_peering.tf
@@ -16,7 +16,7 @@ resource "aws_vpc_peering_connection_accepter" "peer-vpc" {
   provider = aws.peer_vpc
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2peer.id
   auto_accept = true
-  count = (/* var.peer_vpc["create_peering"] && */ (! (var.peer_vpc["external_accepter"])))? 1 : 0
+  count = (! (var.peer_vpc["external_accepter"]))? 1 : 0
 }
 
 /**
@@ -31,7 +31,7 @@ resource "aws_route" "peer2primary" {
   route_table_id            = element(var.peer_vpc["rtbl_ids"], count.index)
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2peer.id
-  count = /* var.peer_vpc["create_peering"] && */ (! var.peer_vpc["external_accepter"])? length(var.peer_vpc["rtbl_ids"]) : 0
+  count = (! var.peer_vpc["external_accepter"])? length(var.peer_vpc["rtbl_ids"]) : 0
 }
 
 /**
@@ -45,5 +45,5 @@ resource "aws_route" "primary2peer" {
   route_table_id            = element(var.route_tables, count.index)
   destination_cidr_block    = var.peer_vpc["cidr_block"]
   vpc_peering_connection_id = aws_vpc_peering_connection.primary2peer.id
-  count = /* (var.peer_vpc["create_peering"])? */ length(var.route_tables) // : 0
+  count = length(var.route_tables)
 }
