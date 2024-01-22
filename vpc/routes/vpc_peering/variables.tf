947e96ece52e4aad2cf4b144debfb44d7665db04
Oron Gola <oron.gola@perception-point.io>
2024-01-22 09:45:51 +0200

    fix reference to peer_vpc variable

--- vpc/routes/vpc_peering/variables.tf
+++ vpc/routes/vpc_peering/variables.tf
@@ -1,4 +1,11 @@
 variable "vpc_id" {}
 variable "vpc_cidr_block" {}
 variable "route_tables" { type = list(string) }
-variable "peer_vpc" {}
+variable "peer_vpc" { type = object({
+                external_accepter = bool
+                aws_region = string
+                vpc_id = string
+                rtbl_ids = optional(list(string), [])
+                account_id = string
+                cidr_block = string
+            })}
