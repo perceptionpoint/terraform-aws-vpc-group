a6e894e3549b1d70a6e28c143a6266f0f881a385
Michael Yefet <michael.yefet@perception-point.io>
2024-10-28 15:01:27 +0200

    PM-9999: align condition

--- vpc/subnet_group/output.tf
+++ vpc/subnet_group/output.tf
@@ -1,6 +1,6 @@
 output "subnet_group" {
   value = {
     subnet_ids = aws_subnet.subnet.*.id
-    route_table_id = length(aws_route_table.route_table) > 0 ? aws_route_table.route_table[0].id : aws_route_table.route_table_ignore_routes[0].id
+    route_table_id = var.subnet_group_properties["ignore_routes_changes"] ? aws_route_table.route_table_ignore_routes[0].id : aws_route_table.route_table[0].id
   }
 }
--- vpc/subnet_group/route_table.tf
+++ vpc/subnet_group/route_table.tf
@@ -20,7 +20,7 @@ resource "aws_route_table" "route_table_ignore_routes" {
 
 resource "aws_route_table_association" "route_table_assoc" {
   subnet_id      = element(aws_subnet.subnet.*.id, count.index)
-  route_table_id = length(aws_route_table.route_table) > 0 ? aws_route_table.route_table[0].id : aws_route_table.route_table_ignore_routes[0].id
+  route_table_id = var.subnet_group_properties["ignore_routes_changes"] ? aws_route_table.route_table_ignore_routes[0].id : aws_route_table.route_table[0].id
 
   count = length(var.subnet_group_properties["availability_zones"])
 }
