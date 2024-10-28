6184bde466f95ca4645df215d0dce2741e5eca54
Michael Yefet <michael.yefet@perception-point.io>
2024-10-28 12:48:59 +0200

    PM-9999: added option to ignore route attribute in the route_table object

--- vpc/subnet_group/output.tf
+++ vpc/subnet_group/output.tf
@@ -1,6 +1,6 @@
 output "subnet_group" {
   value = {
     subnet_ids = aws_subnet.subnet.*.id
-    route_table_id = aws_route_table.route_table.id
+    route_table_id = length(aws_route_table.route_table) > 0 ? aws_route_table.route_table[0].id : aws_route_table.route_table_ignore_routes[0].id
   }
 }
